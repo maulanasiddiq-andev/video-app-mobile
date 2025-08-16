013f886ddde18a6e17b08a2e676c2bb32b2ae577
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-08-16 16:33:06 +0700

    update: formatting date

--- lib/utils/format_date.dart
+++ lib/utils/format_date.dart
@@ -1,5 +1,16 @@
 import 'package:intl/intl.dart';
+import 'package:timeago/timeago.dart' as  timeago;
 
 String formatDate(DateTime date) {
-  return DateFormat('dd MMMM yyyy').format(date);
+  String formattedDate = '';
+
+  Duration diff = DateTime.now().difference(date);
+  if (diff.inDays < 7) {
+    timeago.setLocaleMessages('id', timeago.IdMessages());
+    formattedDate = timeago.format(date, locale: 'id');
+  } else {
+    formattedDate = DateFormat('dd/MM/yyyy').format(date);
+  }
+
+  return formattedDate;
 }
